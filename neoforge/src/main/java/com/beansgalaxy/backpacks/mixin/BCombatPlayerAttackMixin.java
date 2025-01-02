2fffb76458fcde75d3e05f3345bf5dac6c1a3280
Beans Galaxy <connerarsenault@gmail.com>
2025-01-02 02:11:10 -0500

    added pseudo annotation

--- neoforge/src/main/java/com/beansgalaxy/backpacks/mixin/BCombatPlayerAttackMixin.java
+++ neoforge/src/main/java/com/beansgalaxy/backpacks/mixin/BCombatPlayerAttackMixin.java
@@ -6,11 +6,13 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(value = PlayerAttackHelper.class)
 public abstract class BCombatPlayerAttackMixin {
       @Shadow private static void setAttributesForOffHandAttack(Player player, boolean useOffHand) {
